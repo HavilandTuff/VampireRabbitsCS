4b662cd72ae8dfa933c4998b90dbdcac38681098
Karol <karol-tabaka@wp.pl>
2019-07-17 17:53:49 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VampireRabbitsCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Rabbit.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+
+namespace VampireRabbitsCS
+{
+public class Rabbit 
+{
+    private readonly string[] names = new string[]
+    {
+        "Susan", "Bob", "Rob", "Adelle"
+    };
+    private readonly string[] colors = new string[]
+    {
+        "white", "brown", "black"
+    };
+    private string _name;
+    private int _age;
+    private bool _isVampire;
+    private readonly string _color;
+
+
+}
+}
